85c94ea6607f1891302fe24007debf825b891455
reiko_iguchi <reiko6122@gmail.com>
2022-01-09 11:13:58 +0900

    second-commit

--- app/Http/Controllers/BuildingController.php
+++ app/Http/Controllers/BuildingController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 class BuildingController extends Controller
 {
     public function building() {
-        return 'building';
+        return '建物です';
     }
     public function buildingRoom ($room) {
         return '部屋番号は'.$room.'です';
